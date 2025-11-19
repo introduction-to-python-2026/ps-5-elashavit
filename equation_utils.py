65848cfc5665e1a3baa4059b4fae1540aec6de9e
elashavit <ela.shavit@gmail.com>
2025-11-19 16:33:29 +0200

    Update equation_utils.py

--- equation_utils.py
+++ equation_utils.py
@@ -25,7 +25,7 @@ def generate_equation_for_element(compounds, coefficients, element):
 def build_equations(reactant_atoms, product_atoms):
     reactant_coeffs = list(symbols(f'a0:{len(reactant_atoms)}'))
     product_coeffs = list(symbols(f'b0:{len(product_atoms)}'))
-    product_coeffs = product_coeffs[:-1] + [Rational(1)]  # last = 1 as rational
+    product_coeffs = product_coeffs[:-1] + [Rational(1)]
 
     equations = []
     for element in ELEMENTS:
@@ -41,6 +41,44 @@ def my_solve(equations, coefficients):
     result = [solution[c] if c in solution else Rational(1) for c in coefficients]
     return result
 
+def balance_reaction(reaction):
+    from string_utils import count_atoms_in_molecule
+
+    reactants_str, products_str = reaction.replace(" ", "").split("->")
+    reactants = reactants_str.split("+")
+    products = products_str.split("+")
+
+    all_molecules = reactants + products
+    atom_maps = [count_atoms_in_molecule(m) for m in all_molecules]
+
+    atoms = sorted({atom for m in atom_maps for atom in m})
+    num_react = len(reactants)
+    num_prod = len(products)
+
+    rows = []
+    for atom in atoms:
+        row = []
+        for i in range(num_react):
+            row.append(atom_maps[i].get(atom, 0))
+        for i in range(num_prod):
+            row.append(-atom_maps[num_react + i].get(atom, 0))
+        rows.append(row)
+
+    A = Matrix(rows)
+    nullspace = A.nullspace()
+    vec = [Rational(x) for x in nullspace[0]]
+
+    for i, x in enumerate(vec):
+        if x != 0:
+            if x < 0:
+                vec = [-c for c in vec]
+            break
+
+    positive_coeffs = [x for x in vec if x > 0]
+    min_coeff = min(positive_coeffs)
+    vec = [x / min_coeff for x in vec]
+
+    return vec
 
 
 
